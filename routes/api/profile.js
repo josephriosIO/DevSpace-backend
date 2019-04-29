a13ca579a14f64646de8cdc8bb7cb8394de76c3a
josephriosio <blankworldwide@gmail.com>
2019-04-29 15:51:04 -0700

    created endpoint for users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // intitalize router
 const router = express.Router();
 
-//Route GET api/profile/test
-//@desc tests profile route
-//@access public
-router.get("/test", (req, res) => {
-  res.json({ msg: "profile works" });
+//Route GET api/profile/me
+//@desc get cur users profile
+//@access private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ message: "no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
