b64bd5f8b2dabf786f8953357c68fda601c08bf0
josephriosio <blankworldwide@gmail.com>
2019-05-29 14:21:01 -0700

    added github profile repo getter

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,8 @@ const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator/check");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
-
+const request = require("request");
+const config = require("config");
 // intitalize router
 const router = express.Router();
 
@@ -301,6 +302,33 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
+
+//Route get api/profile/github/:username
+//@desc get user repos
+//@access public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `http://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" }
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: "no github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
